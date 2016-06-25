15b5fbdfdc4476b98afe2cddb99e911420833e95
YJK <a@conn.cc>
2016-06-25 22:33:15 +0800

    First Commit

---
+++ config.js
@@ -0,0 +1,12 @@
+config = {
+    blogname: 'Slash',
+    sep: ' | ',
+    user: 'mastermay',
+    repo: 'Slash',
+    per_page: 1,
+    duoshuo_id: 'slash'
+}
+
+var duoshuoQuery = {
+    short_name: config['duoshuo_id']
+};
